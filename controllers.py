923532279433fe0584b678c659f5f65f573237a2
Ahmad-Nuriyev <axmed.nuriyev5@gmail.com>
2024-09-07 23:20:58 +0400

    changes

--- controllers.py
+++ controllers.py
@@ -4,6 +4,26 @@ from app import app
 from extensions import db
 
 
-@app.route ('/')
-def main():
-    return render_template ('frontend/templates/detail.html')
+@app.route ('/shop/')
+def shop():
+    return render_template ('shop.html')
+
+@app.route ('/products/<int:id>/')
+def product():
+    return render_template ('detail.html')
+
+@app.route ('/favourites/', methods = ['GET', 'POST'])
+def favourite():
+    return render_template ('favorites.html')
+
+@app.route ('/register/')
+def register():
+    return render_template ('register.html')
+
+@app.route ('/login/')
+def login():
+    return render_template ('login.html')
+
+@app.route ('/contact/')
+def contacts():
+    return render_template ('contact.html')
---
+++ models.py
@@ -0,0 +1,20 @@
+from extensions import db
+
+
+class User (db.Model):
+    id = db.Column (db.Integer, primary_key = True)
+    name = db.Column (db.String(50), nullable = False)
+    surname = db.Column (db.String(50), nullable = False)
+    email = db.Column (db.Text, nullable = False)
+    password = db.Column (db.Text, nullable = False)
+
+    created_at = db.Column (db.Datetime, server_default = db.func.now())
+    updated_at = db.Column (db.Datetime, server_default = db.funk.now(), server_onupdate = db.func.now())
+
+
+class Product (db.Model):
+    id = db.Column (db.Integer, primary_key = True)
+    name = db.Column (db.String(50), nullable = False)
+    price = db.Column (db.Integer, nullable = False)
+    discount_price = db.Column (db.Integer, nullable = False)
+    photo = db.Column (db.Text, nullable = False)
