fe196ce7ca31db9c6b3f5399ed4ec4b6b428216d
Ahmad-Nuriyev <axmed.nuriyev5@gmail.com>
2024-09-11 17:43:40 +0400

    restarted SQL-Alchemy

--- models.py
+++ models.py
@@ -44,7 +44,7 @@ class Product (db.Model):
     id = db.Column (db.Integer, primary_key = True)
     name = db.Column (db.String(50), nullable = False)
     price = db.Column (db.Integer, nullable = False)
-    discount_price = db.Column (db.Integer, nullable = False)
+    discount_price = db.Column (db.Integer)
     photo = db.Column (db.Text, nullable = False)
 
     created_at = db.Column (db.DateTime, server_default = db.func.now())
